8669a7e78c8966b357a88602c95bca6ac357602d
SerheyBr <sergeydrilkov@mail.ru>
2025-07-09 00:21:41 +0300

    add slider

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: ".", // корень проекта (где index.html)
@@ -8,10 +9,19 @@ export default defineConfig({
     handlebars({
       partialDirectory: path.resolve(__dirname, "src/components"),
     }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "lib/swiper/*",
+          dest: "lib/swiper",
+        },
+      ],
+    }),
   ],
   build: {
     outDir: "dist", // куда собирается проект
     emptyOutDir: true,
+    minify: false,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
